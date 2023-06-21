acf3b121777be69ffd408d9cbef151855fc4a634
Christiane Gross <chgross.wickede@gmail.com>
2023-06-21 13:29:37 +0200

    do more changes to division of cf functions

--- morefunctions.R
+++ morefunctions.R
@@ -99,3 +99,41 @@ multiplycfbootstrap <- function(cf, central, bootstraps){
     }
     return(cfnew)
 }
+
+dividebyreal.cf <- function(cf1, cf2) {
+    stopifnot(inherits(cf1, 'cf_meta'))
+    stopifnot(inherits(cf2, 'cf_meta'))
+    cf <- cf1
+    if(inherits(cf1, 'cf_orig') && inherits(cf2, 'cf_orig')) {
+      stopifnot(all(dim(cf1$cf) == dim(cf2$cf)))
+      stopifnot(cf1$Time == cf2$Time)
+      
+      cf$cf <- cf1$cf / cf2$cf
+      
+      if( has_icf(cf1) | has_icf(cf2) ){
+        stopifnot(has_icf(cf1) & has_icf(cf2))
+        cf$icf <- cf1$icf / cf2$cf
+      }
+    }
+    ## the following is a bit dangerous, however, for
+    ## principal correlators this is the only way to
+    ## build a ratio
+    if(inherits(cf1, 'cf_boot') && inherits(cf2, 'cf_boot') &&
+       all(dim(cf1$cf.tsboot$t) == dim(cf2$cf.tsboot$t)) &&
+       cf1$seed == cf2$seed && cf1$boot.l == cf2$boot.l) {
+      
+      cf$cf.tsboot$t  <- cf1$cf.tsboot$t / cf2$cf.tsboot$t
+      cf$cf.tsboot$t0 <- cf1$cf.tsboot$t0 / cf2$cf.tsboot$t0
+      cf$tsboot.se <- apply(cf$cf.tsboot$t, MARGIN = 2L, FUN = cf$error_fn)
+      cf$cf0 <- cf$cf.tsboot$t0
+      if( has_icf(cf1) | has_icf(cf2) ){
+        stopifnot(has_icf(cf1) & has_icf(cf2))
+        cf$icf.tsboot$t  <- cf1$icf.tsboot$t / cf2$cf.tsboot$t
+        cf$icf.tsboot$t0 <- cf1$icf.tsboot$t0 / cf2$cf.tsboot$t0
+        cf$itsboot.se <- apply(cf$icf.tsboot$t, MARGIN = 2L, FUN = cf$error_fn)
+        cf$icf0 <- cf$icf.tsboot$t0
+      }
+    }
+    else cf <- invalidate.samples.cf(cf)
+    return (cf)
+  }
